4be2884ebacf87e5bed45e591ab2c3d174ddd77e
Piotr-Setsko <lihadzey@gmail.com>
2019-09-30 17:08:50 +0300

    feat: write function, that decode Morse code

--- src/index.js
+++ src/index.js
@@ -39,6 +39,48 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arr = [];
+    let arr2 = [];
+    let i = 0;
+
+    arr = expr.split('');
+
+    while(arr.length != 0) {
+    arr2[i] = arr.splice(0,10);
+    i+=1;
+    }
+
+    for (i=0; i<arr2.length; i++) {
+      for(let j=0; j < arr2[i].length; j++) {
+        if ((arr2[i][j]+arr2[i][j+1]) == "00") {
+          arr2[i].splice(j,2);
+          j=j-1;
+        } else if ((arr2[i][j]+arr2[i][j+1]) == "10") {
+          arr2[i].splice(j,2,".");
+        } else if ((arr2[i][j]+arr2[i][j+1]) == "11") {
+          arr2[i].splice(j,2, "-");
+        } else if ((arr2[i][j]+arr2[i][j+1]) == "**") {
+          arr2[i] = " ";
+        }
+      }
+      if (arr2[i] != " ") {
+      arr2[i] = arr2[i].join('');
+      }
+    }
+
+  let map = Object.entries(MORSE_TABLE);
+
+  for (i=0; i<arr2.length; i++) {
+    for(let j=0; j < map.length; j++) {
+      if (arr2[i] == map[j][0]) {
+        arr2[i] = map[j][1];
+      }
+    }
+  }
+
+  let decode = arr2.join('');
+
+  return decode;
 }
 
 module.exports = {
